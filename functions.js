73cee15809f0d23649d04b1e818fb57c94326ca0
samar <samoramahmoud22@gmail.com>
2020-04-13 03:26:57 +0200

    add function to read and write file

--- functions.js
+++ functions.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
+const filePath = "./todo.json";
+
 
 function readData (pathFile){
     const todos = fs.readFileSync(pathFile,'utf8') 
     return JSON.parse(todos)
 }
-function writeData (){
 
+function writeData (data){
+    const jasonData = fs.stringify(data); 
+    fs.writeFileSync(filePath,jasonData)
 }
 
 function add(option){
-
+     
 }
 
 
